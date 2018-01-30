cdacd7f7efbca67ed709f9ecfce2247e6a677a46
Hiren Patel <hirenpatel@tejs-macbook-air.local>
2018-01-29 17:13:45 -0800

    Initial Commit

---
+++ Week1-LabTests/Week1_LabTests.swift
@@ -0,0 +1,36 @@
+//
+//  Week1_LabTests.swift
+//  Week1-LabTests
+//
+//  Created by Hiren Patel on 1/29/18.
+//  Copyright © 2018 Hiren Patel. All rights reserved.
+//
+
+import XCTest
+@testable import Week1_Lab
+
+class Week1_LabTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
